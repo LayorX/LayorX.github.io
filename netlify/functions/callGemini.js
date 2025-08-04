34a3ae7f15844642719499222b0bd4f87a86986c
lalawu9453 <lalawu499@gmail.com>
2025-08-04 10:09:07 +0800

    L

--- netlify/functions/callGemini.js
+++ netlify/functions/callGemini.js
@@ -9,13 +9,14 @@ exports.handler = async function(event, context) {
     const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
 
+
     const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${GEMINI_API_KEY}`;
 
     try {
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: userPrompt
+            body: JSON.stringify(userPrompt)
         });
 
         if (!response.ok) {
